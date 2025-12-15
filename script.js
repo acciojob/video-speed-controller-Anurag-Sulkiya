21cffd3911005bf2d42e85163191ede3fe722574
Anurag Sulkiya <163811905+Anurag-Sulkiya@users.noreply.github.com>
2025-12-16 01:44:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,75 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+const video = document.querySelector('.flex');
+const progress = document.querySelector('.progress');
+const progressbar = document.querySelector('.progress__filled');
+
+function handleProgress(){
+    const percentage = (video.currentTime/ video.duration) * 100;
+	progressbar.style.width = `${percentage}%`;
+}
+
+video.addEventListener('timeupdate', handleProgress);
+
+function scrub(e){
+    const scrubTime = (e.offsetX/progress.offsetWidth) * video.duration;
+	video.currentTime = scrubTime;
+}
+
+progress.addEventListner('click', scrub);
+
+let mousedown = false;
+progress.addEventListner('mousemove', (e)=> mousedown && scrub(e));
+progress.addEventListner('mousedown', (e)=> mousedown = true);
+progress.addEventListner('mouseup', (e)=> mousedown = false);
+
+const toggle = document.querySeletor('.player__button.toggle');
+
+function togglePlay(){
+  if(video.paused){
+     video.play();
+  }
+  else{
+    video.pause();
+  }
+}
+
+function updateButton(){
+   const icon = video.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+toggle.addEventListner('click', togglePlay);
+video.addEventListner('play', updateButton);
+video.addEventListner('pause', updateButton);
+video.addEventListner('click', togglePlay);
+
+const skipButtons = document.querySelectorAll('[data-skip]');
+function skip() {
+	video.currentTime += parseFloat(this.dataset.skip);
+}
+skipButtons.forEach(button => button.addEventListner('click',skip));
+
+const volumeInput = document.querySelector('.volume');
+const volumeDisplay = document.querySelector('.volume-display');
+
+function handleVolumeUpdate(){
+	video.volume = this.value;
+	volumeDisplay.textContent = Math.round(this.value * 100) + %;
+}
+
+volumeInput.addEventListener('input', handleVolumeUpdate);
+volumeDisplay.textContent ='100%';
+
+const speedInput = document.querySelector('.playbackSpeed');
+const speedDisplay =document.querySelector('.speed-display');
+
+function handleSpeedUpdate() {
+	video.playbackRate = this.value;
+	speedDisplay.textContent = parseFloat(this.value).toFixed(1) + 'x';
+}
+
+speedInput.addEventListner('input',  handleSpeedUpdate);
+speedDisplay.textContent = '1.0x';
